764c38bb6d5ec3ce71b1a5afbc953db847d9b8d8
unsup <collazosdie@unicauca.edu.co>
2024-01-25 16:29:57 -0500

    user auth validation to client from server

--- src/services/user.ts
+++ src/services/user.ts
@@ -4,7 +4,7 @@ interface IGetUserData {
 export async function getUserData({ email }: IGetUserData) {
   try {
     const data = await fetch(
-      process.env.API_URL +
+      process.env.BASE_URL_EXTENSION_API +
         "/v1/user/data?username=" +
         email.split("@")[0]
     );
@@ -12,6 +12,7 @@ export async function getUserData({ email }: IGetUserData) {
       const aux = await data.json();
       return aux;
     }
+    return false;
   } catch (error) {
     return false;
   }
