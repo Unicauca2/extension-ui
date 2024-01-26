3c2215b4f368e301cfbaefbe7123240ae0796166
unsup <collazosdie@unicauca.edu.co>
2024-01-25 21:56:55 -0500

    pagina subir oferta académica eliminada, no se usará

--- src/app/(app)/(authorized)/functionary/academicOffer/page.tsx
+++
@@ -1,9 +0,0 @@
-import CSVUploader from "./components/CSVUploader";
-
-export default function AcademicOffer() {
-  return (
-    <div className="m-4 pb-10 bg-[#ffffff] w-full rounded-3xl">
-      <CSVUploader />
-    </div>
-  );
-}
